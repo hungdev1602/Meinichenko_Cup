b7557dde2c4c531ecd420935815fdb6da992d45b
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-12-03 20:47:29 +0300

    Add responsive (Organizations page)

--- src/pages/Organizers/Organizers.jsx
+++ src/pages/Organizers/Organizers.jsx
@@ -2,34 +2,41 @@ import melnichenko from "/images/melnichenko.png"
 import founder from "/images/founder.png"
 import Section2 from "./sections/Section2"
 const Organizers = () => {
+  const text = ` занимается развитием 
+  промышленных городов 
+и территорий в разных регионах России. 
+Основан предпринимателем Андреем 
+Мельниченко.
+`
   return (
     <>
-      <div className="mt-[60px] mb-[130px]">
-        <div className="w-[427px] h-auto truncate mx-auto">
+      <div className="mt-[40px] sm:mt-[60px] mb-[90px] sm:mb-[130px]">
+        <div className="w-[214px] sm:w-[340px] md:w-[427px] h-auto truncate mx-auto">
           <img src={melnichenko} alt="" className="w-full h-full object-cover"/>
         </div>
 
-        <div className="mt-[90px] flex justify-between gap-[90px]">
+        <div className="mt-[40px] sm:mt-[60px] md:mt-[90px] flex justify-between flex-wrap md:flex-nowrap gap-[15px] sm:gap-[30px] md:gap-[90px]">
           {/* Left */}
-          <div className="w-[42%] text-[24px]">
-            <div className=""><span className="font-actay"><b>Фонд Мельниченко</b></span> занимается развитием промышленных городов и территорий в разных регионах России.</div>
-            <div className="mt-[25px] ">Основан предпринимателем Андреем Мельниченко.</div>
+          <div className="w-[100%] md:w-[42%] text-[14px] sm:text-[18px] lg:text-[24px] text-center md:text-start">
+            <div className="hidden sm:block "><span className="font-actay"><b>Фонд Мельниченко</b></span> занимается развитием промышленных городов и территорий в разных регионах России.</div>
+            <div className="hidden sm:block mt-[25px] ">Основан предпринимателем Андреем Мельниченко.</div>
+            <div className="block sm:hidden text-[14px] text-center whitespace-pre-line"><b className="font-actay">Фонд Мельниченко</b> {text}</div>
           </div>
 
           {/* Right */}
-          <div className="flex-1">
-            <div className="w-[565px] h-auto rounded-[20px] truncate">
+          <div className="w-[100%] md:flex-1">
+            <div className="w-full md:w-[455px] lg:w-[565px] h-auto rounded-[20px] truncate">
               <img src={founder} alt="" className="w-full h-full object-cover"/>
             </div>
-            <div className="text-[20px] font-[400] mt-[10px]">Андрей Мельниченко</div>
+            <div className="hidden sm:block text-[16px] md:text-[20px] font-[400] text-center md:text-start mt-[10px]">Андрей Мельниченко</div>
           </div>
         </div>
 
-        <div className="mt-[-80px] w-fit">
+        <div className="mt-[10px] md:mt-[-80px] mx-auto md:mx-0 w-fit">
           <a 
             href="https://aimfond.ru" 
             target="_blank" 
-            className="inline-block w-[393px] h-[74px] bg-[#111821] py-[22px] px-[35px] rounded-[15px] text-center text-[24px] font-actay"
+            className="inline-block w-[232px] sm:w-[270px] lg:w-[393px] h-[40px] sm:h-[62px] lg:h-[74px] bg-[#111821] py-[10px] sm:py-[22px] px-[21px] sm:px-[35px] rounded-[10px] sm:rounded-[15px] text-center text-[14px] lg:text-[24px] font-actay"
           >
             перейти на сайт фонда
           </a>
--- src/pages/Organizers/sections/Section2.jsx
+++ src/pages/Organizers/sections/Section2.jsx
@@ -20,42 +20,42 @@ const Section2 = () => {
   инициатив`
   return (
     <>
-      <div className="mt-[120px]">
-        <div className="text-[32px] font-[700] font-actay text-center uppercase">Фонд реализует проекты в следующих сферах: </div>
+      <div className="mt-[50px] md:mt-[120px]">
+        <div className="text-[14px] sm:text-[20px] md:text-[32px] font-[700] font-actay text-center uppercase">Фонд реализует проекты в следующих сферах: </div>
 
-        <div className="mt-[58px]">
-          <div className="grid grid grid-cols-4 gap-[20px]">
+        <div className="mt-[20px] sm:mt-[58px]">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[10px] sm:gap-[20px]">
             {/* Item 1 */}
-            <div className="w-[275px] h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase">Образование</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase">Образование</div>
             </div>
             {/* Item 2 */}
-            <div className="w-[275px] h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase">медицина</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase">медицина</div>
             </div>
             {/* Item 3 */}
-            <div className="w-[275px] h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text1}</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text1}</div>
             </div>
             {/* Item 4 */}
-            <div className="w-[275px] h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text2}</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[110px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text2}</div>
             </div>
             {/* Item 5 */}
-            <div className="w-[275px] h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text3}</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text3}</div>
             </div>
             {/* Item 6 */}
-            <div className="w-[275px] h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text4}</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text4}</div>
             </div>
             {/* Item 7 */}
-            <div className="w-[275px] h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text5}</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text5}</div>
             </div>
             {/* Item 8 */}
-            <div className="w-[275px] h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
-              <div className="text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text6}</div>
+            <div className="w-[160px] sm:w-[230px] xl:w-[275px] h-[90px] sm:h-[128px] border border-[#FF5200] rounded-[10px] flex items-center justify-center">
+              <div className="text-[12px] sm:text-[16px] md:text-[20px] font-[500] uppercase whitespace-pre-line text-center">{text6}</div>
             </div>
           </div>
         </div>
